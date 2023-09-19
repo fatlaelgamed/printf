8743833185324699908c14cd2cb63bde2a48a90f
fatlaelgamed <saifelhady2004@gmail.com>
2023-09-18 22:12:56 -0700

    project

--- main.h
+++ main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 #include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <stdio.h>
 
 int _printf(const char *format, ...);
 int print_char(int c);
