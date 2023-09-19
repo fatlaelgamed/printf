24707bf1a94a75e411d57313ce20a36923e05ade
fatlaelgamed <saifelhady2004@gmail.com>
2023-09-18 22:01:47 -0700

    project

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_h
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int _printf(const char *format, ...);
+int print_char(int c);
+int print_digit(long n, int base);
+int print_str(char *s);
+int print_format(char specifier, va_list ap);
+#endif 
